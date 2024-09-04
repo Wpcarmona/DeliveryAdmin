10937fa186475040e4a3e694b6ef551c2009398b
Pedro Carmona <wpcarmona@unicesar.edu.co>
2024-09-04 16:57:34 -0500

    add chart

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,15 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    provideCharts(withDefaultRegisterables())
+  ]
 };
