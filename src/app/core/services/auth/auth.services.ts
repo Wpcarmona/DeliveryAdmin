a1763c0e6d0977e865130719b72b21e620a76c06
Pedro Carmona <wpcarmona@unicesar.edu.co>
2024-09-11 10:57:46 -0500

    add login post

--- src/app/core/services/auth/auth.services.ts
+++ src/app/core/services/auth/auth.services.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthResponse, User } from '../../models/user.models';
+import { environment } from '../../../../environments/environments';
 
 
 @Injectable({
     providedIn: 'root', // Indica que este servicio está disponible a nivel global
   })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/deliveryshop/auth/login'; // Reemplaza {{url}} con tu URL base
+  private baseUrl = environment.apiBaseUrl;
+  private apiUrl = `${this.baseUrl}/deliveryshop/auth/login`; 
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCosGIRdXOzuFqUqZb3WUgFFBRHzTEdTkA",
   authDomain: "deliveryshop-a675d.firebaseapp.com",
   projectId: "deliveryshop-a675d",
@@ -7,4 +7,12 @@ const firebaseConfig = {
   messagingSenderId: "749216487824",
   appId: "1:749216487824:web:2933df1c0cf1836071bb15",
   measurementId: "G-8LL2H7EGLP"
+};
+
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://backenddelivery.onrender.com',
+  timeout: 3000, // Tiempo de espera para las peticiones en milisegundos
+  retryAttempts: 3, // Número de reintentos en caso de fallo
+  loggingEnabled: true, // Habilitar/deshabilitar logs
 };
