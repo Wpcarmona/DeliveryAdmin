0f4283c6e73b6357866f13f4e04ff549107d8d6c
Pedro Carmona <wpcarmona@unicesar.edu.co>
2024-09-11 12:33:34 -0500

    add configurate login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { CardComponent, LoaderComponent, NavbarComponent } from './shared/components';
 import { HomeComponent } from "./pages/home/home.component";
@@ -21,15 +21,30 @@ import { StateService } from './shared/utils/stateservices/state.services';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DeliveryAdmin';
   isLoginPage = false;
   isLoading = false;
+  isSessionActive = false;
 
   constructor(private stateService: StateService, private router: Router) {
     this.router.events.subscribe(() => {
       this.isLoginPage = this.router.url === '/login';
     });
+  }
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.stateService.setIsLoading(true);
+    if(!this.isSessionActive){
+      this.isLoading = false;
+      this.stateService.setIsLoading(false);
+      this.router.navigate(['/login'])
+    }else {
+      this.isLoading = false;
+      this.stateService.setIsLoading(false);
+      this.router.navigate(['/'])
+    }
+
   }
 
   showLoader() {
