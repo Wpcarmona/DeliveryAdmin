7bf4e031e36dbb42b7f446060029d5d3a1f32917
Pedro Carmona <wpcarmona@unicesar.edu.co>
2024-09-11 16:00:10 -0500

    add axios

--- src/app/core/services/auth/auth.services.ts
+++ src/app/core/services/auth/auth.services.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { AuthResponse, User } from '../../models/user.models';
+import axios from 'axios';
+import { Observable, from } from 'rxjs';
+import { AuthResponse } from '../../models/user.models';
 import { environment } from '../../../../environments/environments';
-
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-    providedIn: 'root', // Indica que este servicio está disponible a nivel global
-  })
+  providedIn: 'root',
+})
 export class AuthService {
   private baseUrl = environment.apiBaseUrl;
-  private apiUrl = `${this.baseUrl}/deliveryshop/auth/login`; 
+  private apiUrl = `${this.baseUrl}/deliveryshop/auth/login`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private cookieService: CookieService) {}
 
-  login(email: string, password: string): Observable<AuthResponse> {
+  login(email: string, password: string): Observable<any> {
     const body = { email, password };
 
-    return this.http.post<AuthResponse>(this.apiUrl, body);
+    // Convertir la promesa de Axios a un Observable usando `from`
+    return from(
+      axios
+        .post<AuthResponse>(this.apiUrl, body) // withCredentials habilita el envío de cookies
+        .then(response => response.data)
+    );
+  }
+
+  // Método para recuperar el token desde la cookie
+  getToken(): string | null {
+    return this.cookieService.get('token');
   }
 }
